3a2c3d1997087332f8c0f0827742051dab8c87f1
Elder Jefferson, Livisi Carbajal <elderlivisi@gmail.com>
2022-10-14 00:52:05 -0500

    Add files via upload

---
+++ Entregable_Tema1/src/com/Tema1.java
@@ -0,0 +1,21 @@
+package com;
+
+public class Tema1 {
+    public static void main(String[] args) {
+
+        int number = 5;
+        long people = 41241515 ;
+        double salary = 5500.6;
+        boolean is_active = true;
+        String name = "Elder Livisi";
+
+        System.out.println(number);
+        System.out.println(people);
+        System.out.println(salary);
+        System.out.println(is_active);
+        System.out.println(name);
+
+
+
+    }
+}
